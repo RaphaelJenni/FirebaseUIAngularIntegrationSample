752c8b82bc16204cd7b7ae33f711180cee1427d3
Raphael Jenni <raphael.jenni@comerge.net>
2017-02-10 16:08:35 +0100

    Cleanup code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, AfterViewInit} from "@angular/core";
 import {AngularFire} from "angularfire2";
+import User = firebase.User;
 const firebase = require('firebase');
 const firebaseui = require('firebaseui');
 
@@ -10,51 +11,31 @@ const firebaseui = require('firebaseui');
 })
 export class AppComponent implements AfterViewInit {
 
+  private firebaseUiInstance;
   title = 'app works!';
 
+
   constructor(private aF: AngularFire) {
+    // Initialize the FirebaseUI Widget using Firebase.
+    this.firebaseUiInstance = new firebaseui.auth.AuthUI(firebase.auth());
+
   }
 
   ngAfterViewInit(): void {
 
+    //Only fire this if the user isn't authenticated
     this.aF.auth.subscribe(value => {
       if (!value) this.firebaseUIPopup();
     });
 
-
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        user.getToken().then(function (accessToken) {
-          document.getElementById('account-details').textContent = JSON.stringify({
-            displayName: displayName,
-            email: email,
-            emailVerified: emailVerified,
-            photoURL: photoURL,
-            uid: uid,
-            accessToken: accessToken,
-            providerData: providerData
-          }, null, '  ');
-        });
-      } else {
-        document.getElementById('account-details').textContent = 'null - User not signed in';
-      }
-    }, function (error) {
-      console.log(error);
-    });
+    firebase.auth().onAuthStateChanged(this.firebaseAuthChangeListener, (error) => console.log(error));
 
   }
 
   firebaseUIPopup() {
     let uiConfig = {
       callbacks: {
-        signInSuccess: function (currentUser, credential, rederictUrl) {
+        signInSuccess: (currentUser, credential, rederictUrl) => {
           console.log(currentUser);
           return false;
         }
@@ -72,10 +53,26 @@ export class AppComponent implements AfterViewInit {
       tosUrl: '<your-tos-url>'
     };
 
-    // Initialize the FirebaseUI Widget using Firebase.
-    let ui = new firebaseui.auth.AuthUI(firebase.auth());
     // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig);
+    this.firebaseUiInstance.start('#firebaseui-auth-container', uiConfig);
+  }
+
+  firebaseAuthChangeListener(user: User) {
+    if (user) {
+      user.getToken().then(function (accessToken) {
+        document.getElementById('account-details').textContent = JSON.stringify({
+          displayName: user.displayName,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          photoURL: user.photoURL,
+          uid: user.uid,
+          accessToken: accessToken,
+          providerData: user.providerData
+        }, null, '  ');
+      });
+    } else {
+      document.getElementById('account-details').textContent = 'null - User not signed in';
+    }
   }
 
   logout() {
